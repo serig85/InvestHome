f6628574656e16aefbde5c11bb090eb02b3dfac6
serig85 <115493415+serig85@users.noreply.github.com>
2024-06-08 17:19:44 +0300

    application of linter

--- page/main_page.py
+++ page/main_page.py
@@ -1,36 +1,45 @@
+"""Main page"""
+import os
+import json
+from time import sleep
+
 from selene import query
-from selene.api import by, be, have
+from selene.api import have
 from selene.support.shared import config
 from selenium.webdriver import Keys
 
 from static.data import data
-
 from .locators import LocatorsMainPage, LocatorPopUpWindows
-from time import sleep
-import os
-import json
 
-datapath = './static/data/stedent.txt'
-impath = './static/image/dtjxK5Y9Bt8.jpg'
-impath_abs = os.path.abspath(impath)
-im_file_name = impath.split('/')[-1]
-page_url = 'https://demoqa.com/automation-practice-form'
 
-datadict = {'fist_name': 'Serg',
-            'last_name': 'Sper',
-            'email': 'test@test.te',
-            'gender': 'Male',
-            'mobile_num': '0123456789',
-            'date_of_birch': '08 Aug 1985',
-            'hobbies': ['Sports', 'Music'],
-            'subjects': "subg_text",
-            'file_name': impath_abs,
-            'current_address': 'krasniy pahar',
-            'state': 'Uttar Pradesh',
-            'city': 'Lucknow'}
+config.reports_folder = 'reports'
+DATA_PATH = './static/data/stedent.txt'
+IMPATH = './static/image/dtjxK5Y9Bt8.jpg'
+IMPATH_ABS = os.path.abspath(IMPATH)
+
+
+datadict = {'fist_name': 'Serg',  # Любой текст
+            'last_name': 'Sper',  # Любой текс
+            'email': 'test@test.te',  # Адрес электронной почты (проверяется по шаблону)
+            'gender': 'Male',  # Одно из трёх Male, Female, Other
+            'mobile_num': '0123456789',  # 10 цифр
+            'date_of_birch': '08 Aug 1985',  # формат число из 2 цифр пробел месяц сокращенно пробел год 4 цифры
+            'hobbies': ['Sports', 'Music'],  # Список составленный из одного/нескольки
+            # выбранных из: Sports,Reading,Music
+            'subjects': "subg_text",  # Любой текс
+            'file_name': IMPATH_ABS,  # Абсолютный путь к файлу изображения.
+            'current_address': 'krasniy pahar',  # Любой текс
+            'state': 'Uttar Pradesh',  # открыть на странице список выбор штата и выбрать один.
+            'city': 'Lucknow'}  # После выбора штата открыть список городов и выбрать один.
+
+
+def save_data():
+    with open(DATA_PATH, 'w') as file:
+        json.dump(datadict, file)
 
 
 def precondition_function(page):
+    """ Running before tests """
     page.element(LocatorsMainPage.fist_name_locator).send_keys(datadict['fist_name'])
     page.element(LocatorsMainPage.last_name_locator).send_keys(datadict['last_name'])
     page.element(LocatorsMainPage.email_locator).send_keys(datadict['email'])
@@ -41,7 +50,7 @@ def precondition_function(page):
     dn = dob.get(query.value)
     dob.set_value(dk)
     dob.send_keys(Keys.HOME)
-    for i in dn:
+    for _ in dn:
         dob.send_keys(Keys.DELETE)
     dob.press_escape()
 
@@ -65,6 +74,7 @@ def precondition_function(page):
 
 
 def postcondition_function(page):
+    """Running after tests"""
     sleep(10)
     page.element(LocatorPopUpWindows.button_locator).click()
     sleep(5)
@@ -82,7 +92,8 @@ def mobile(page):
 
 
 def email(page):
-    page.element(LocatorPopUpWindows.student_email_locator).should(have.exact_text(datadict['email']))
+    (page.element(LocatorPopUpWindows.student_email_locator).
+     should(have.exact_text(datadict['email'])))
 
 
 def mobile_neg(page):
@@ -107,17 +118,22 @@ def subjects(page):
 
 def hobbies(page):
     list_hobbies = page.element(LocatorPopUpWindows.hobbies_locator).get(query.text).split(' ')
-    set_hobbies = set([i.removesuffix(',') for i in list_hobbies])
+    list_hobbies = [i.removesuffix(',') for i in list_hobbies]
     for hobbi in datadict['hobbies']:
-        set_hobbies.remove(hobbi)
-    assert len(set_hobbies) == 0
+        list_hobbies.remove(hobbi)
+    assert len(list_hobbies) == 0
 
 
 def picture(page):
-    page.element(LocatorPopUpWindows.picture_locator).should(have.exact_text(datadict['file_name'].split('\\')[-1]))
+    (page.element(LocatorPopUpWindows.picture_locator).
+     should(have.exact_text(datadict['file_name'].rsplit('\\', maxsplit=1)[-1])))
 
 
 def current_address(page):
-    page.element(LocatorPopUpWindows.address_locator).should(have.exact_text(datadict['current_address']))
+    (page.element(LocatorPopUpWindows.address_locator).
+     should(have.exact_text(datadict['current_address'])))
+
+
 def state_city(page):
-    page.element(LocatorPopUpWindows.state_and_city_locator).should(have.exact_text(f'{datadict['state']} {datadict['city']}'))
+    (page.element(LocatorPopUpWindows.state_and_city_locator).
+     should(have.exact_text(f'{datadict['state']} {datadict['city']}')))
--- test_all.py
+++ test_all.py
@@ -1,13 +1,18 @@
 import allure
 import selene
-from page.main_page import *
+from page.main_page import (save_data, precondition_function, postcondition_function,
+                            student_name, email, gender, mobile, mobile_neg,
+                            date_of_birch, subjects, hobbies, picture,
+                            current_address, state_city)
 
 PAGE_URL = 'https://demoqa.com/automation-practice-form'
+
 
 class TestPage:
 
     def setup_class(self):
         print('hello')
+        save_data()
         self.page = selene.browser.open(PAGE_URL)
         precondition_function(self.page)
 
@@ -16,14 +21,17 @@ class TestPage:
         postcondition_function(self.page)
 
     @allure.title("Test fist/last name")
+    @allure.story("Flickering test")
     def test_student_name(self):
         student_name(self.page)
 
     @allure.title("Test email")
+    @allure.story("Good test")
     def test_email(self):
         email(self.page)
 
     @allure.title("Test gender")
+    @allure.story("Good test")
     def test_gender(self):
         gender(self.page)
 
@@ -32,30 +40,37 @@ class TestPage:
     def test_mobile(self):
         mobile(self.page)
 
-    @allure.title("Bed test")
+    @allure.title("Bad test mobile rigged")
+    @allure.story("Bad test")
     def test_mobile_neg(self):
         mobile_neg(self.page)
 
     @allure.title("Test date_of_birch")
+    @allure.story("Good test")
     def test_date_of_birch(self):
         date_of_birch(self.page)
 
     @allure.title("Test subjects")
+    @allure.story("Bad test")
     def test_subjects(self):
         subjects(self.page)
 
     @allure.title("Test hobbies")
+    @allure.story("Good test")
     def test_hobbies(self):
         hobbies(self.page)
 
     @allure.title("Test picture")
+    @allure.story("Good test")
     def test_picture(self):
         picture(self.page)
 
     @allure.title("Test current_address")
+    @allure.story("Good test")
     def test_current_address(self):
         current_address(self.page)
 
     @allure.title("Test state and city")
+    @allure.story("Good test")
     def test_state_city(self):
         state_city(self.page)
